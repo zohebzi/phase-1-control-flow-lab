1f97e93c5b4ff45fe0a0b81e01a2b4ac5363ba5d
Zoheb Iqbal <zoheb.zafar.iqbal@gmail.com>
2022-04-26 13:33:29 -0400

    commit tree 7da3e59424f2529e96d6507a4eb8dbfcae04576c

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+  return 'This one is on me!';
+} 
+  else if (ride > 2000 && ride < 2500){
+  return 'I will gladly take your thirty bucks.';
+}
+  else if (ride > 2500){
+  return 'No can do.';
+}
+} 
+
+function ternaryCheckCity(city){
+ return (city === 'NYC') ? "Ok, sounds good.": "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default: 
+      return 'Bye.'
+    
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
 }
